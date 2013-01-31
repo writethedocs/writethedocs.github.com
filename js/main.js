77bbe802adcfd9737d5cbd976286004767c743d8
Eric Redmond <eric.redmond@gmail.com>
2013-01-31 15:17:03 -0800

    Add speakers and sponsors

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-
+$(function() {
+  /*
+  $("#speakers .speakers .media a").modal({
+    ajax: false,
+    beforeShow: function() {
+      var details = this.$trigger.siblings(".modal").clone()
+      var c = this.$el.find(".inner").empty();
+      console.log(details.html())
+      details.appendTo(c);
+      $("<figure />", { html: details.html() }).prependTo(c);
+    }
+  });
+  */
+});
